97d50cfb68e830fcbf4e2732bda641eafc607c56
Riley Bathurst <riley@rileybathurst.com>
2021-07-13 14:30:58 -0700

    webpack to parcel

---
+++ app.js
@@ -0,0 +1,32 @@
+/* import plogo from "./images/PACRAV.png";
+var logodiv = document.getElementById("logo");
+
+import physio from "./images/MG_7625_bw-medium.jpg";
+var physiodiv = document.getElementById("hero");
+
+function component() {
+  const element = document.createElement("div"); // this is messy but it stops errors so it should be fixed
+
+  const myIcon = new Image();
+  myIcon.src = plogo;
+  myIcon.alt = "pacrav logo";
+
+  element.appendChild(myIcon);
+  return element;
+}
+
+logodiv.appendChild(component());
+
+function picture() {
+  const diiv = document.createElement("div"); // this is messy but it stops errors so it should be fixed
+
+  const myImage = new Image();
+  myImage.src = physio;
+  myImage.alt = "matt working on a patient";
+
+  diiv.appendChild(myImage);
+  return diiv;
+}
+
+physiodiv.appendChild(picture());
+ */
